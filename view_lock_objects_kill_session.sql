f5c5095a4e9df49a70ede659ed64f8839d14148b
lgorkemt <gorkemt@hotmail.com>
2016-08-05 14:59:23 +0300

    Create view_lock_objects_kill_session.sql

---
+++ view_lock_objects_kill_session.sql
@@ -0,0 +1,22 @@
+
+-- shows locked objects
+select
+   c.owner,
+   c.object_name,
+   c.object_type,
+   b.sid,
+   b.serial#,
+   b.status,
+   b.osuser,
+   b.machine
+from
+   v$locked_object a ,
+   v$session b,
+   dba_objects c
+where
+   b.sid = a.session_id
+and
+   a.object_id = c.object_id;
+
+-- kills session
+ALTER SYSTEM KILL SESSION 'sid,serial#';
